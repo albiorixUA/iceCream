7ab9ed60e22c7c42876742e1bf51634713eefc64
VSRudnyk <86798079+VSRudnyk@users.noreply.github.com>
2021-10-09 06:05:57 +0000

    Deploying to gh-pages from @ VSRudnyk/iceCream@13770027bc452c13a8c347d6d37a6d4c9d0591c4 🚀

--- src.b361e033.js
+++ src.b361e033.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/iceCream/src.6dc3aaa8.js.map
+//# sourceMappingURL=/iceCream/src.b361e033.js.map
