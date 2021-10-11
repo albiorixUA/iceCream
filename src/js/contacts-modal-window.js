d615fc993f8af1407854bb35cfaad68d3545ef06
IhorKozloff <ik.eyeshield21@gmail.com>
2021-10-11 13:42:47 +0300

    section contacts
    Вроде как все закончил. Если кто-то свежим взглядом найдет косяк, буду благодарен и рад исправить)

--- src/js/contacts-modal-window.js
+++ src/js/contacts-modal-window.js
@@ -1,10 +1,18 @@
 const contactsButtons = document.getElementById('contactsModalBbtnWrapper');
 const contactsBtnArr = document.querySelectorAll('.contacts__modal-btn');
 const contactsContentArr = document.querySelectorAll('.modal-maps__content-item');
-const modalLocation = document.getElementById('location-btn');
+const modalLocationBtn = document.getElementById('location-btn');
+const modalShopsBtn = document.getElementById('franchise-btn');
 const modalOverlay = document.querySelector('.modal-overlay');
-const contactsCloseBtn = document.getElementById('contactsCloseBtn');
+const modalMaps = document.querySelector('.modal-maps');
+const modalFranchise = document.querySelector('.modal-franchise');
 
+const contactsMapsCloseBtn = document.getElementById('contactsMapsCloseBtn');
+const contactsFranchiseCloseBtn = document.getElementById('contactsFranchiseCloseBtn');
+
+
+
+// табы
 const changeClass = el => {
     for (o = 0; o < contactsBtnArr.length; o++)  {
         contactsBtnArr[o].classList.remove('active');
@@ -12,11 +20,23 @@ const changeClass = el => {
     el.classList.add('active');
 };
 
-modalLocation.addEventListener('click', ()=> {
+modalLocationBtn.addEventListener('click', ()=> {
     modalOverlay.classList.add('active');
+    modalMaps.classList.add('active');
+
 });
-contactsCloseBtn.addEventListener('click', ()=> {
+modalShopsBtn.addEventListener('click', ()=> {
+    modalOverlay.classList.add('active');
+    modalFranchise.classList.add('active');
+
+});
+contactsMapsCloseBtn.addEventListener('click', ()=> {
     modalOverlay.classList.remove('active');
+    modalMaps.classList.remove('active');   
+});
+contactsFranchiseCloseBtn.addEventListener('click', ()=> {
+    modalOverlay.classList.remove('active');
+    modalFranchise.classList.remove('active');   
 });
 
 
@@ -34,3 +54,60 @@ contactsButtons.addEventListener('click', qui => {
         };
     };
 });
+
+// Слайдер
+
+const prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
+      slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot');
+
+let index = 0;
+const activeSlide = n => {
+for(slide of slides) {
+    slide.classList.remove('active');
+}
+slides[n].classList.add('active');
+}
+const activeDot = n => {
+    for(dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+    }
+
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0;
+        activeSlide(index);
+        activeDot(index);
+    }   else {
+            index++;
+            activeSlide(index);
+            activeDot(index);
+        }
+
+}
+const prevSlide = () => {
+    if (index == 0) {
+        index = slides.length - 1;
+        activeSlide(index);
+        activeDot(index);
+    }   else {
+            index--;
+            activeSlide(index);
+            activeDot(index);
+        }
+
+}
+dots.forEach((item, indexDot)=> {
+item.addEventListener('click', () => {
+    index = indexDot;
+    activeSlide(index);
+    activeDot(index);
+})
+})
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
